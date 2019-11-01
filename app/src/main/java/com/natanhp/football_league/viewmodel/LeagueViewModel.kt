643dfb8278cc400219bb8835e9f7006807a532fd
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-02 02:00:40 +0700

    Create league's model
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/natanhp/football_league/viewmodel/LeagueViewModel.kt
@@ -0,0 +1,11 @@
+package com.natanhp.football_league.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.natanhp.football_league.model.LeagueModel
+import com.natanhp.football_league.repository.LeagueRepository
+
+class LeagueViewModel : ViewModel() {
+    val leagueRepository = LeagueRepository()
+    val leagueModel: LiveData<ArrayList<LeagueModel>> = leagueRepository.getLeague()
+}
