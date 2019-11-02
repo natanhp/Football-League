b02761b6c9d74e540bb74c57644ce3a9a3e02de9
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-03 02:42:34 +0700

    Create a parcelable model
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/model/LeagueModel.kt
+++ app/src/main/java/com/natanhp/football_league/model/LeagueModel.kt
@@ -1,11 +1,15 @@
 package com.natanhp.football_league.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class LeagueModel(
     private val id: Int,
     private val name: String,
     private val logo: Int,
     private val description: String
-) {
+) : Parcelable {
     fun getId() = id
     fun getName() = name
     fun getLogo() = logo
