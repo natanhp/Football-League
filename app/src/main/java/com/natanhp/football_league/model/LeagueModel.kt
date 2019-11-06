f3fa7d107429d677fa427449e8b438cb0a6134a8
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-06 23:39:14 +0700

    Add serializename annotation and a new attribute The serializename annotation is for deserialization/serialization process The new attribute is onlineLogo to store the URL logo from the API
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/model/LeagueModel.kt
+++ app/src/main/java/com/natanhp/football_league/model/LeagueModel.kt
@@ -1,17 +1,28 @@
 package com.natanhp.football_league.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class LeagueModel(
+    @SerializedName("idLeague")
     private val id: Int,
+
+    @SerializedName("strLeague")
     private val name: String,
+
     private val logo: Int,
+
+    @SerializedName("strBadge")
+    private val onlineLogo: String,
+
+    @SerializedName("strDescriptionEN")
     private val description: String
 ) : Parcelable {
     fun getId() = id
     fun getName() = name
     fun getLogo() = logo
     fun getDesc() = description
+    fun getOnlineLogo() = onlineLogo
 }
