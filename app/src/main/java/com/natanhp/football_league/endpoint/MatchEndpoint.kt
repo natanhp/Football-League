81eeed9eb903ba626aaf71514cfba0b9edcca011
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-09 01:01:10 +0700

    Create endpoint interface for match
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/natanhp/football_league/endpoint/MatchEndpoint.kt
@@ -0,0 +1,15 @@
+package com.natanhp.football_league.endpoint
+
+import com.natanhp.football_league.model.MatchModels
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MatchEndpoint {
+
+    @GET("api/v1/json/1/eventsnextleague.php")
+    fun getNextMatches(@Query("id") id: Int?): Call<MatchModels>
+
+    @GET("api/v1/json/1/eventspastleague.php")
+    fun getPreviousMatches(@Query("id") id: Int): Call<MatchModels>
+}
