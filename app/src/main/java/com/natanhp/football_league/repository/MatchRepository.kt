f076271fd5b6841eaba9316be15cefae38a7de33
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-09 01:02:05 +0700

    Create repository for match
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/natanhp/football_league/repository/MatchRepository.kt
@@ -0,0 +1,33 @@
+package com.natanhp.football_league.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.natanhp.football_league.api.RetrofitService
+import com.natanhp.football_league.endpoint.MatchEndpoint
+import com.natanhp.football_league.model.MatchModels
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MatchRepository {
+    private val nextMatches = MutableLiveData<MatchModels>()
+    private val prevMatches = MutableLiveData<MatchModels>()
+    val retrofit2 = RetrofitService.getRetrofitInstance()
+
+    fun getNextMatches(id: Int?): LiveData<MatchModels> {
+        val nextMatchEndpoit = retrofit2.create(MatchEndpoint::class.java)
+
+        nextMatchEndpoit.getNextMatches(id).enqueue(object : Callback<MatchModels> {
+            override fun onFailure(call: Call<MatchModels>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call<MatchModels>, response: Response<MatchModels>) {
+                nextMatches.postValue(response.body())
+            }
+
+        })
+
+        return nextMatches
+    }
+}
