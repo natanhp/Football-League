c5993ee16c646ff026e47a95d94f32dc125705f4
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-09 10:54:30 +0700

    Add function to request data for search match
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/repository/MatchRepository.kt
+++ app/src/main/java/com/natanhp/football_league/repository/MatchRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.natanhp.football_league.api.RetrofitService
 import com.natanhp.football_league.endpoint.MatchEndpoint
 import com.natanhp.football_league.model.MatchModels
+import com.natanhp.football_league.model.MatchModelsSearch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,6 +13,8 @@ import retrofit2.Response
 class MatchRepository {
     private val nextMatches = MutableLiveData<MatchModels>()
     private val prevMatches = MutableLiveData<MatchModels>()
+    private val searchMatches = MutableLiveData<MatchModelsSearch>()
+
     val retrofit2 = RetrofitService.getRetrofitInstance()
 
     fun getNextMatches(id: Int?): LiveData<MatchModels> {
@@ -46,5 +49,26 @@ class MatchRepository {
         })
 
         return prevMatches
+    }
+
+    fun searchMatches(query: CharSequence): LiveData<MatchModelsSearch> {
+        val nextMatchEndpoit = retrofit2.create(MatchEndpoint::class.java)
+
+        nextMatchEndpoit.searchMatches(query.toString())
+            .enqueue(object : Callback<MatchModelsSearch> {
+                override fun onFailure(call: Call<MatchModelsSearch>, t: Throwable) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onResponse(
+                    call: Call<MatchModelsSearch>,
+                    response: Response<MatchModelsSearch>
+                ) {
+                    searchMatches.postValue(response.body())
+                }
+
+            })
+
+        return searchMatches
     }
 }
