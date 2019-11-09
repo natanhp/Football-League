601a0c2ec47897ecbe37f335604f8a4021e34140
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-09 10:53:37 +0700

    Reformat the code
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/model/MatchModels.kt
+++ app/src/main/java/com/natanhp/football_league/model/MatchModels.kt
@@ -1,14 +1,14 @@
 package com.natanhp.football_league.model
 
-import com.natanhp.football_league.modeldata.MatchModel
 import com.google.gson.annotations.SerializedName
+import com.natanhp.football_league.modeldata.MatchModel
 
 
 data class MatchModels(
 
     @SerializedName("events")
     private val matches: ArrayList<MatchModel>
-){
+) {
     fun getMatches(): ArrayList<MatchModel> {
         return matches
     }
