5331379402f352fa03e238d1b4ea1e553ac42c97
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-09 09:15:45 +0700

    Reformat the code
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/view/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/natanhp/football_league/view/ui/main/SectionsPagerAdapter.kt
@@ -18,7 +18,7 @@ private val TAB_TITLES = arrayOf(
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     FragmentPagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment = when(position){
+    override fun getItem(position: Int): Fragment = when (position) {
         0 -> NextMatchFragment.newInstance(position + 1)
         1 -> PrevMatchFragment.newInstance(position + 1)
         else -> NextMatchFragment.newInstance(position + 1)
