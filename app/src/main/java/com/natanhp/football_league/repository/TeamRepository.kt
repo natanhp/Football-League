ff82e8f099939f670df1c1a1bbdbb8e3681b4121
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-11 00:41:25 +0700

    Create repository for team
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/natanhp/football_league/repository/TeamRepository.kt
@@ -0,0 +1,35 @@
+package com.natanhp.football_league.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.natanhp.football_league.api.RetrofitService
+import com.natanhp.football_league.endpoint.TeamEndpoint
+import com.natanhp.football_league.model.TeamModel
+import com.natanhp.football_league.model.TeamModels
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class TeamRepository {
+    private val team = MutableLiveData<ArrayList<TeamModel>>()
+
+    private val retrofit2 = RetrofitService.getRetrofitInstance()
+
+    fun getTeamDetail(id: Int): LiveData<ArrayList<TeamModel>> {
+
+        val endpoint = retrofit2.create(TeamEndpoint::class.java)
+        endpoint.getTeamDetail(id).enqueue(object : Callback<TeamModels> {
+
+            override fun onResponse(call: Call<TeamModels>, response: Response<TeamModels>) {
+                team.postValue(response.body()?.teams)
+            }
+
+            override fun onFailure(call: Call<TeamModels>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
+        return team
+    }
+}
