3950fe742bd731e8f3c54cd8bbb32ca06974daa7
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-11 00:45:51 +0700

    Create image view for team logo
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/view/MatchDetailActivity.kt
+++ app/src/main/java/com/natanhp/football_league/view/MatchDetailActivity.kt
@@ -2,37 +2,65 @@ package com.natanhp.football_league.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.natanhp.football_league.R
+import com.natanhp.football_league.model.TeamModel
 import com.natanhp.football_league.modeldata.MatchModel
+import com.natanhp.football_league.viewmodel.TeamViewModel
 
 class MatchDetailActivity : AppCompatActivity() {
+
+    lateinit var teamViewModel: TeamViewModel
+    private val teamModel = ArrayList<TeamModel>()
+    lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_detail)
 
-        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
+        teamViewModel = ViewModelProviders.of(this).get(TeamViewModel::class.java)
+
+        progressBar = findViewById(R.id.progress_bar)
+
         progressBar.visibility = View.VISIBLE
 
+        val match: MatchModel? = intent.getParcelableExtra("match_detail")
 
-        val match: MatchModel = intent.getParcelableExtra("match_detail")
+
         match?.let {
             title = "${it.teamHome} vs ${it.teamAway}"
-            initViews(it)
-            progressBar.visibility = View.GONE
+            val matchModel = it
+            teamViewModel.getDetailTeam(matchModel.homeTeamId).observe(this, Observer {
+                val teamModelTmp = it
+                teamModelTmp.let {
+                    teamModel.add(teamModelTmp[0])
+                    teamViewModel.getDetailTeam(matchModel.awayTeamId).observe(this, Observer {
+                        if (teamModelTmp[0].idTeam != it[0].idTeam) {
+                            teamModel.add(it[0])
+                            initViews(matchModel)
+                        }
+
+
+                    })
+                }
+            })
         }
     }
 
-    private fun initViews(match: MatchModel){
+    private fun initViews(match: MatchModel) {
         val homeTeamName: TextView = findViewById(R.id.text_home_team)
         val homeTeamScore: TextView = findViewById(R.id.text_home_score)
         val homeTeamGoalDetail: TextView = findViewById(R.id.text_home_goal_detail)
         val homeTeamRedCard: TextView = findViewById(R.id.text_home_red_card)
         val homeTeamYellowCard: TextView = findViewById(R.id.text_home_yellow_card)
         val homeTeamLineUpGoalKeeper: TextView = findViewById(R.id.text_home_line_up_goal_keeper)
+        val homeTeamLogo: ImageView = findViewById(R.id.logo_home)
 
         val awayTeamName: TextView = findViewById(R.id.text_away_team)
         val awayTeamScore: TextView = findViewById(R.id.text_away_score)
@@ -40,6 +68,7 @@ class MatchDetailActivity : AppCompatActivity() {
         val awayTeamRedCard: TextView = findViewById(R.id.text_away_red_card)
         val awayTeamYellowCard: TextView = findViewById(R.id.text_away_yellow_card)
         val awayTeamLineUpGoalKeeper: TextView = findViewById(R.id.text_away_line_up_goal_keeper)
+        val awayTeamLogo: ImageView = findViewById(R.id.logo_away)
 
         homeTeamName.text = match.teamHome
         homeTeamScore.text = match.homeScore.toString()
@@ -48,11 +77,22 @@ class MatchDetailActivity : AppCompatActivity() {
         homeTeamYellowCard.text = match.homeYellowCard
         homeTeamLineUpGoalKeeper.text = match.homeLineUpGoalKeeper
 
+        Glide.with(this)
+            .load(teamModel[0].teamLogo)
+            .into(homeTeamLogo)
+
+
         awayTeamName.text = match.teamAway
         awayTeamScore.text = match.awayScore.toString()
         awayTeamGoalDetail.text = match.awayGoalDetail
         awayTeamRedCard.text = match.awayRedCard
         awayTeamYellowCard.text = match.awayYellowCard
         awayTeamLineUpGoalKeeper.text = match.awayLineUpGoalKeeper
+
+        Glide.with(this)
+            .load(teamModel[1].teamLogo)
+            .into(awayTeamLogo)
+
+        progressBar.visibility = View.GONE
     }
 }
