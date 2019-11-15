1298719638584d485d776e5a1d060c2081784da4
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-15 18:02:47 +0700

    Add new functions and change the class data type Add showFavoriteMatches() and getMatchDetail() and change the data type to AndroidViewModel so it can be aware of the application context
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/viewmodel/MatchViewModel.kt
+++ app/src/main/java/com/natanhp/football_league/viewmodel/MatchViewModel.kt
@@ -1,12 +1,16 @@
 package com.natanhp.football_league.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import com.natanhp.football_league.repository.MatchRepository
 
-class MatchViewModel : ViewModel() {
+class MatchViewModel(application: Application) : AndroidViewModel(application) {
     private val matchRepository = MatchRepository()
 
     fun getNextMatches(id: Int?) = matchRepository.getNextMatches(id)
     fun getPrevMatches(id: Int?) = matchRepository.getPrevMatches(id)
     fun searchMatches(query: CharSequence) = matchRepository.searchMatches(query)
+    fun showFavoriteMatches(context: Context) = matchRepository.showFavorite(context)
+    fun getMatchDetail(matchID: Int?) = matchRepository.getMatchDetail(matchID)
 }
