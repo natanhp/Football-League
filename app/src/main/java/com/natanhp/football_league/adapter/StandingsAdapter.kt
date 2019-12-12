1220697fc2b8c65485419c6f68e69f4820e04cb2
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-12-12 15:35:52 +0700

    Create an adapter for standings
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/natanhp/football_league/adapter/StandingsAdapter.kt
@@ -0,0 +1,59 @@
+package com.natanhp.football_league.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.natanhp.football_league.R
+import com.natanhp.football_league.model.StandingsModel
+
+class StandingsAdapter : RecyclerView.Adapter<StandingsAdapter.ViewHolder>() {
+    private var standings = ArrayList<StandingsModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.standing_item,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int = standings.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binder(standings[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val teamName: TextView = itemView.findViewById(R.id.textViewTeamName)
+        private val played: TextView = itemView.findViewById(R.id.textViewPlayed)
+        private val goalsFor: TextView = itemView.findViewById(R.id.textViewGoalsFor)
+        private val goalsAgainst: TextView = itemView.findViewById(R.id.textViewGoalsAgainst)
+        private val goalsDifference: TextView = itemView.findViewById(R.id.textViewGoalsDifference)
+        private val win: TextView = itemView.findViewById(R.id.textViewWin)
+        private val draw: TextView = itemView.findViewById(R.id.textViewDraw)
+        private val loss: TextView = itemView.findViewById(R.id.textViewLoss)
+        private val total: TextView = itemView.findViewById(R.id.textViewTotal)
+
+        fun binder(standings: StandingsModel) {
+            teamName.text = standings.teamName
+            played.text = standings.played.toString()
+            goalsFor.text = standings.goalsFor.toString()
+            goalsAgainst.text = standings.goalsAgainst.toString()
+            goalsDifference.text = standings.goalsDifference.toString()
+            win.text = standings.win.toString()
+            draw.text = standings.draw.toString()
+            loss.text = standings.loss.toString()
+            total.text = standings.total.toString()
+        }
+    }
+
+    fun setStandings(standings: ArrayList<StandingsModel>) {
+        this.standings.clear()
+        this.standings.addAll(standings)
+        notifyDataSetChanged()
+    }
+
+}
