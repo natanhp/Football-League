e24f97980ae3ec6df6c01c9ffc477a7f6cc7cf7c
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-12-12 15:38:47 +0700

    Create standing activity
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/natanhp/football_league/view/StandingsActivity.kt
@@ -0,0 +1,44 @@
+package com.natanhp.football_league.view
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.natanhp.football_league.R
+import com.natanhp.football_league.adapter.StandingsAdapter
+import com.natanhp.football_league.viewmodel.StandingsViewModel
+
+class StandingsActivity : AppCompatActivity() {
+
+    private val standingsViewModel by lazy {
+        ViewModelProviders.of(this).get(StandingsViewModel::class.java)
+    }
+
+    private lateinit var progressBar: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_standings)
+        title = getString(R.string.standings)
+
+        progressBar = findViewById(R.id.progress_bar)
+        val standingsAdapter = StandingsAdapter()
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = standingsAdapter
+
+        progressBar.visibility = View.VISIBLE
+        standingsViewModel.getStandings(intent.getIntExtra("idLeague", 0)).observe(this, Observer {
+            it.let {
+                standingsAdapter.setStandings(it.standings)
+                progressBar.visibility = View.GONE
+            }
+        })
+    }
+
+
+}
