11718a2290de358ad440b5586ca8ab21165b8bd1
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-12-14 19:49:50 +0700

    Create new table for favorite team
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/db/MyDatabaseOpenHelper.kt
+++ app/src/main/java/com/natanhp/football_league/db/MyDatabaseOpenHelper.kt
@@ -3,6 +3,7 @@ package com.natanhp.football_league.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import com.natanhp.football_league.model.FavoriteMatchModel
+import com.natanhp.football_league.model.TeamModel
 import org.jetbrains.anko.db.*
 
 class MyDatabaseOpenHelper(ctx: Context) :
@@ -31,10 +32,26 @@ class MyDatabaseOpenHelper(ctx: Context) :
             FavoriteMatchModel.AWAY_TEAM_NAME to TEXT,
             FavoriteMatchModel.AWAY_TEAM_SCORE to TEXT
         )
+
+        p0?.createTable(
+            TeamModel.TABLE_NAME, true,
+            TeamModel.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+            TeamModel.TEAM_ID to TEXT + UNIQUE,
+            TeamModel.TEAM_LOGO to TEXT,
+            TeamModel.TEAM_NAME to TEXT,
+            TeamModel.ALTERNATE_NAME to TEXT,
+            TeamModel.FORMED_YEAR to TEXT,
+            TeamModel.STADIUM to TEXT,
+            TeamModel.STADIUM_DESCRIPTION to TEXT,
+            TeamModel.STADIUM_LOCATION to TEXT,
+            TeamModel.TEAM_DESCRIPTION to TEXT,
+            TeamModel.SPORT_TYPE to TEXT
+        )
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         p0?.dropTable(FavoriteMatchModel.TABLE_FAVORITE, true)
+        p0?.dropTable(TeamModel.TABLE_NAME, true)
     }
 
 }
