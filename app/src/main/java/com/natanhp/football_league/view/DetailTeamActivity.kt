f26045c373654c7a291121eaa3a8df068e251111
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-12-17 10:15:18 +0700

    Add some code so it can be use to add/remove team to fav list
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/natanhp/football_league/view/DetailTeamActivity.kt
+++ app/src/main/java/com/natanhp/football_league/view/DetailTeamActivity.kt
@@ -1,20 +1,47 @@
 package com.natanhp.football_league.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.natanhp.football_league.R
 import com.natanhp.football_league.model.TeamModel
+import com.natanhp.football_league.viewmodel.TeamViewModel
 
 class DetailTeamActivity : AppCompatActivity() {
+
+    private val teamViewModel by lazy {
+        ViewModelProviders.of(this).get(TeamViewModel::class.java)
+    }
+
+    private var isFavorite: Boolean = false
+    private var menuItem: Menu? = null
+    private lateinit var progressBar: ProgressBar
+    private val team: TeamModel by lazy {
+        intent.getParcelableExtra<TeamModel>("team")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_team)
 
-        setViews(intent.getParcelableExtra("team"))
+        setViews(team)
+
+        progressBar = findViewById(R.id.progress_bar)
+
+        team.let {
+            title = it.teamName
+        }
+
+        getFavoriteState()
     }
 
     private fun setViews(team: TeamModel?) {
@@ -38,5 +65,68 @@ class DetailTeamActivity : AppCompatActivity() {
         stadiumDescription.text = team?.stadiumDescription ?: ""
         stadiumLocation.text = team?.stadiumLocation ?: ""
         teamDescription.text = team?.teamDescription ?: ""
+    }
+
+    private fun getFavoriteState() {
+        progressBar.visibility = View.VISIBLE
+        team.let {
+            teamViewModel.getFavoriteStateTeam(this, it.idTeam.toLong()).observe(this, Observer {
+                isFavorite = !it.isNullOrEmpty()
+            })
+            progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.favorite_menu, menu)
+        menuItem = menu
+        setFavorite()
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+
+                true
+            }
+
+            R.id.add_to_favorite -> {
+                if (isFavorite) {
+                    removeFromFavorite()
+                } else {
+                    addToFavorite()
+                }
+
+                isFavorite = !isFavorite
+                setFavorite()
+
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun addToFavorite() {
+        team.let { teamViewModel.addToFavoriteTeam(this, it) }
+    }
+
+    private fun removeFromFavorite() {
+        team.let {
+            teamViewModel.removeFromFavoriteTeam(this, it.idTeam.toLong())
+        }
+    }
+
+    private fun setFavorite() {
+        if (isFavorite) {
+            menuItem?.getItem(0)?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_added_to_favorites)
+        } else {
+            menuItem?.getItem(0)?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_add_to_favorites)
+        }
     }
 }
